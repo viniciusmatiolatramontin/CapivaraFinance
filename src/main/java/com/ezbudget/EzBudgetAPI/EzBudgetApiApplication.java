0ca60e02b347d1383e81a5b5fee66329619bb23e
Vinícius Matiola Tramontin <viniciusmatiolatramontin@gmail.com>
2024-03-17 09:02:14 -0300

    Created repository

---
+++ src/main/java/com/ezbudget/EzBudgetAPI/EzBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.ezbudget.EzBudgetAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzBudgetApiApplication.class, args);
+	}
+
+}
